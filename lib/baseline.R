c988db02b8f42b118740c1082fc3e27b17c9f8f2
Yuhan Sun <ys2883@columbia.edu>
2016-03-22 19:04:07 -0400

    Added files via upload

--- lib/baseline.R
+++ lib/baseline.R
@@ -1,20 +1,23 @@
+library(e1071)
 #################################################################################
 # trainx_b <- the training data set
 # trainblabel_b <- labels of training dada
 # testx_b <- the test data set
 #################################################################################
-load("~/Desktop/Git Repository/cycle3cvd-team4/data/Baseline_features.RData")
+load("~/Desktop/cycle3cvd-team4/data/Baseline_features.RData")
 trainb=Initial
-trainx_b<-trainb[-1,]
-trainlabel_b <- as.data.frame(read.table("label_base.txt"),header=T)
+trainx_b<-as.matrix(trainb)[-1,-1]
+trainlabel_b <- as.data.frame(read.table("~/Desktop/cycle3cvd-team4/data/label_base.txt"),header=T)
 trainlabel_b <- as.matrix(trainlabel_b)[-1,]
 
 #Using to do cross validation
 set.seed(1)
 ind=sample(1:dim(trainx_b)[1],200)
-traintest_b=subset(trainx_b[ind,],select=-1)
+traintest_b=trainx_b[ind,]
+traintest_b=matrix(as.numeric(traintest_b),nrow=200,ncol=800)
 ttlabel_b<-trainlabel_b[ind,2]
 ttdata_b <- cbind(ttlabel_b,traintest_b)
+
 
 #Using to train linear SVM
 trainx1_b <- subset(trainx_b[1:7000,],select=-1)
@@ -26,8 +29,10 @@ testlabel_b<-trainlabel_b[7001:7377,2]
 #################################################################################
 # obj_b <- Cross-validate the margin parameter, k=10
 #################################################################################
-
-obj_b <- tune.svm(ttlabel_b ~., data = ttdata_b, cost = seq(1,5,by=0.5),type="C-classification", kernel = "linear",sampling="cross",cross=10)
+x=as.data.frame(traintest_b)
+y=as.data.frame(ttlabel_b)
+da1=cbind(y,x)
+obj_b <- tune.svm(ttlabel_b~.,data=da1,cost = seq(0.1,1,by=0.01),type="C-classification", kernel = "linear",sampling="cross",cross=10)
 summary(obj_b)
 plot(obj_b)
 
