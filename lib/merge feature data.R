d28371b4ba1e3d98d5cd4719f66afc9a5d1052fc
sukizqy0402 <qz2246@columbia.edu>
2016-03-23 10:04:08 -0400

    Update merge feature data.R

--- lib/merge feature data.R
+++ lib/merge feature data.R
@@ -2,7 +2,9 @@ data_adv<-read.table("~/Downloads/featrues2.txt")
 load("Baseline_features.RData")
 
 data_bs=Initial[-1,]
-data_bs1=matrix(as.numeric(data_bs[,-1]),nrow=7376,ncol=800)
+nr=dim(data_bs)[1]
+nc=dim(data_bs)[2]-1
+data_bs1=matrix(as.numeric(data_bs[,-1]),nrow=nr,ncol=nc)
 newdata<-cbind(data_bs[,1],data_bs1)
 
 
